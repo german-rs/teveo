cfe2cfbe1f4cd47234988ffafdca7ad2f874a6df
german-rs <ing.germanriveros@gmail.com>
2025-04-23 15:40:42 -0400

    Componente BrowserDetector - funcionalidad terminada

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import BrowserDetector from './components/BrowserDetector.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BrowserDetector,
+  },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="header">
+    <h1>Te Veo 👁</h1>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <BrowserDetector />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.header {
+  text-align: center;
 }
 </style>
---
+++ src/components/BrowserDetector.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="browser-detector">
+    <h2>Información del Navegador</h2>
+    <p>
+      Estas usando: <strong>{{ browserName }}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BrowserDetector',
+  data() {
+    return {
+      browserName: '',
+    }
+  },
+  mounted() {
+    this.detectBrowser()
+  },
+  methods: {
+    detectBrowser() {
+      const userAgent = navigator.userAgent
+
+      if (userAgent.match(/chrome|Chromium|crios/i)) {
+        this.browserName = 'Google Chrome'
+      } else if (userAgent.match(/firefox|fxios/i)) {
+        this.browserName = 'Mozilla Firefox'
+      } else if (userAgent.match(/safari/i)) {
+        this.browserName = 'Safari'
+      } else if (userAgent.match(/opr\//i)) {
+        this.browserName = 'Opera'
+      } else if (userAgent.match(/edg/i)) {
+        this.browserName = 'Microsoft Edge'
+      } else {
+        this.browserName = 'Navegador desconocido'
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.browser-detector {
+  padding: 20px;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
+h2 {
+  color: #2c3e50;
+}
+p {
+  color: #34495e;
+}
+strong {
+  color: #42b983;
+}
+</style>
